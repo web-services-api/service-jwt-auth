681572c100a202d30066153443746cfc817a3283
Alexis Herold <alexis.herold13@gmail.com>
2024-04-25 13:43:00 +0200

    cleanup

--- config.js
+++ config.js
@@ -1,12 +1,19 @@
+const crypto = require('crypto');
+
+const generateRandomString = (length) => {
+  return crypto.randomBytes(Math.ceil(length / 2))
+    .toString('hex')
+    .slice(0, length);
+};
+
 module.exports = {
-    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
-    refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
-    dbConfig: {
-      user: process.env.DB_USERNAME,
-      host: process.env.DB_HOST,
-      database: process.env.DB_DATABASE,
-      password: process.env.DB_PASSWORD,
-      port: process.env.DB_PORT,
-    },
-  };
-  
+  accessTokenSecret: generateRandomString(64),
+  refreshTokenSecret: generateRandomString(64),
+  dbConfig: {
+    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  },
+};
